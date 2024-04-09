1c859105319598e4ba85adec9ae9cca8532804cc
Marcelo Comanduci <mcomanduci@gmail.com>
2024-04-09 17:47:10 -0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b979fa42-076b-4300-8904-5bb5bc04dad1';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/paginationView.js
@@ -0,0 +1,70 @@
+import icons from 'url:../../img/icons.svg';
+import View from './View';
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+      if (!btn) return;
+
+      const goToPage = +btn.dataset.goto;
+
+      handler(goToPage);
+    });
+  }
+
+  _generateMarkup() {
+    const curPage = this._data.page;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+
+    // Page 1, and more pages
+    if (curPage === 1 && numPages > 1) {
+      return this.nextButton();
+    }
+
+    // last page
+    if (curPage === numPages && numPages > 1) {
+      return this.previousButton();
+    }
+
+    // other page
+    if (curPage < numPages) {
+      return `${this.nextButton()} ${this.previousButton()}`;
+    }
+
+    // Page 1 and no more
+    return '';
+  }
+
+  nextButton() {
+    return `
+    <button data-goto="${
+      this._data.page + 1
+    }" class="btn--inline pagination__btn--next">
+      <span>Page ${this._data.page + 1}</span>
+      <svg class="search__icon">
+        <use href="${icons}#icon-arrow-right"></use>
+      </svg>
+    </button>
+    `;
+  }
+
+  previousButton() {
+    return `
+    <button data-goto="${
+      this._data.page - 1
+    }" class="btn--inline pagination__btn--prev">
+      <svg class="search__icon">
+        <use href="${icons}#icon-arrow-left"></use>
+      </svg>
+      <span>Page ${this._data.page - 1}</span>
+    </button>;
+    `;
+  }
+}
+
+export default new PaginationView();
